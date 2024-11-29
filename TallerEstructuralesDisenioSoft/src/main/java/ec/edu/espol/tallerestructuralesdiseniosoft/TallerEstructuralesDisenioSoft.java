8f4e888bb5283ca34ee202cdf0b0b46ed725ffbc
MARLO VALLE <mcvalle@espol.edu.ec>
2024-11-28 21:19:21 -0500

    subido el doc a chambiar

---
+++ TallerEstructuralesDisenioSoft/src/main/java/ec/edu/espol/tallerestructuralesdiseniosoft/TallerEstructuralesDisenioSoft.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.tallerestructuralesdiseniosoft;
+
+/**
+ *
+ * @author marsa
+ */
+public class TallerEstructuralesDisenioSoft {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
