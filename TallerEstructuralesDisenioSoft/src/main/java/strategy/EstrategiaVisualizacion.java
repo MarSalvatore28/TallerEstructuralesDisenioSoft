b9e7ff96e8e97dfc34f71fd85b0cac6d80f7760d
Daniela Reyes <152106996+dannreye@users.noreply.github.com>
2024-11-28 22:15:59 -0500

    tablero

--- TallerEstructuralesDisenioSoft/src/main/java/strategy/EstrategiaVisualizacion.java
+++ TallerEstructuralesDisenioSoft/src/main/java/strategy/EstrategiaVisualizacion.java
@@ -1,4 +1,8 @@
+package strategy;
+
 import java.util.List;
+
+import FactoryMethod.Tarea;
 
 public interface EstrategiaVisualizacion{
     void mostrarTareas(List<Tarea> tareas);
---
+++ TallerEstructuralesDisenioSoft/src/main/java/strategy/VistaTablero.java
@@ -0,0 +1,13 @@
+package strategy;
+
+import java.util.List;
+
+import FactoryMethod.Tarea;
+
+public class VistaTablero implements EstrategiaVisualizacion {
+    
+    @Override
+    public void mostrarTareas(List<Tarea> tareas) {
+        // Lógica para mostrar las tareas en formato tablero
+    }
+}
